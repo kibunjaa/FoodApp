3d84b24ef0ba4fd9a77a8fc9d9d7b8009fce5943
kibunjaa <kibunjafranziska@gmail.com>
2022-08-29 12:07:24 +0300

    First Commit

--- app/src/main/java/com/example/foodapp/Activity2.kt
+++ app/src/main/java/com/example/foodapp/Activity2.kt
@@ -1,8 +1,7 @@
 package com.example.foodapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/foodapp/IntroActivity.kt
+++ app/src/main/java/com/example/foodapp/IntroActivity.kt
@@ -3,7 +3,6 @@ package com.example.foodapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
