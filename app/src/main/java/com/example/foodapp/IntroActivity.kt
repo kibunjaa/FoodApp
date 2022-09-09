aebf5e07f50a7125fe8a8271b4acbc29ae945e50
kibunjaa <kibunjafranziska@gmail.com>
2022-09-09 11:54:01 +0300

    First Commit

--- app/src/main/java/com/example/foodapp/IntroActivity.kt
+++ app/src/main/java/com/example/foodapp/IntroActivity.kt
@@ -3,25 +3,27 @@ package com.example.foodapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 
-class IntroActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class IntroActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-/*
+        setContentView(R.layout.activity_intro)
+
         Handler().postDelayed({
-            val intent = Intent(this,Activity2::class.java)
+            val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000
 
         )
 
- */
         val buttonClick = findViewById<Button>(R.id.button_click)
-        buttonClick.setOnClickListener {
-            val intent = Intent(this, Activity2::class.java)
+        buttonClick.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.foodapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Activity2 : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
 
 
     }
